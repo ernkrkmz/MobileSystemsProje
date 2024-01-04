11bb219dd5f1994d1bec94cb51f3f82409888d2a
Ernkrkmz <eren_korkmaz_@hotmail.com>
2024-01-04 22:29:47 +0300

    enes3

--- MobileProject/navigation/appNavigation.js
+++ MobileProject/navigation/appNavigation.js
@@ -7,44 +7,46 @@ import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
 
+
 const Tab = createBottomTabNavigator();
+
 
 export default function appNavigation() {
     return (
         <NavigationContainer>
       <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: 'black',
-        tabBarInactiveTintColor: 'gray',
-        tabBarActiveBackgroundColor: 'lightgrey'
+    screenOptions={{
+      tabBarActiveTintColor: 'black',
+      tabBarInactiveTintColor: 'gray',
+      tabBarActiveBackgroundColor: 'lightgrey'
 
-      }}
-      >
+    }}
+    >
 
-        <Tab.Screen name="Home" component={HomeScreen} options={{
-          tabBarLabel: 'Home',
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarLabel: 'Home',
 
-          tabBarIcon: () => (
-            <Entypo name="home" color={'black'} size={30} />
-          ),
-        }}/>
-        <Tab.Screen name="Search" component={SearchScreen} options={{
-          tabBarLabel: 'Search',
+        tabBarIcon: () => (
+          <Entypo name="home" color={'black'} size={30} />
+        ),
+      }}/>
+      <Tab.Screen name="Search" component={SearchScreen} options={{
+        tabBarLabel: 'Search',
 
-          tabBarIcon: () => (
-            <Ionicons name="md-search-sharp" size={30} color="black" />
-          ),
-        }} />
+        tabBarIcon: () => (
+          <Ionicons name="md-search-sharp" size={30} color="black" />
+        ),
+      }} />
 
-        <Tab.Screen name="deneme" component={MovieScreen} options={{
-          tabBarLabel: 'deneme',
+      <Tab.Screen name="Movie" component={MovieScreen} options={{
+        tabBarLabel: 'Movie',
+        headerShown:0,
+        tabBarIcon: () => (
+          <Ionicons name="md-search-sharp" size={30} color="black" />
+        ),
+      }} /> 
 
-          tabBarIcon: () => (
-            <Ionicons name="md-search-sharp" size={30} color="black" />
-          ),
-        }} /> 
-
-      </Tab.Navigator>
+    </Tab.Navigator>
     </NavigationContainer>
       );
 }
--- MobileProject/screens/MovieScreen.js
+++ MobileProject/screens/MovieScreen.js
@@ -1,13 +1,12 @@
 import { View, Text, Image, Dimensions, TouchableOpacity, ScrollView, Platform } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
-import { LinearGradient } from 'expo-linear-gradient'
 import { ArrowLeftIcon, ChevronLeftIcon } from 'react-native-heroicons/outline';
 import { HeartIcon} from 'react-native-heroicons/solid';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Cast from '../components/cast';
 import MovieList from '../components/movieList';
-import { fallbackMoviePoster, fetchMovieCredits, fetchMovieDetails, fetchSimilarMovies, image500 } from '../api/moviedb';
+import { fallbackMoviePoster, fetchMovieCredits, fetchMovieDetails, fetchSimilarMovies, image342, image500 } from '../api/moviedb';
 import { styles, theme } from '../theme';
 import Loading from '../components/loading';
 
@@ -60,17 +59,17 @@ export default function MovieScreen() {
   return (
     <ScrollView 
         contentContainerStyle={{paddingBottom: 20}} 
-        className="flex-1 bg-neutral-900">
+        >
 
       {/* back button and movie poster */}
-      <View className="w-full">
-        <SafeAreaView className={"absolute z-20 w-full flex-row justify-between items-center px-4 "+topMargin}>
-            <TouchableOpacity style={styles.background} className="rounded-xl p-1" onPress={()=> navigation.goBack()}>
-                <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+      <View >
+        <SafeAreaView>
+            <TouchableOpacity style={{backgroundColor:'gray'}} onPress={()=> navigation.goBack()}>
+                <ChevronLeftIcon size="28" strokeWidth={2.5} color="black" />
             </TouchableOpacity>
 
             <TouchableOpacity onPress={()=> toggleFavourite(!isFavourite)}>
-                <HeartIcon size="35" color={isFavourite? theme.background: 'white'} />
+                <HeartIcon size="35" color={isFavourite? theme.background: '#f0a5cb'} />
             </TouchableOpacity>
         </SafeAreaView>
         {
@@ -79,17 +78,10 @@ export default function MovieScreen() {
             ):(
                 <View>  
                     <Image 
-                        // source={require('../assets/images/moviePoster2.png')} 
-                        source={{uri: image500(movie.poster_path) || fallbackMoviePoster}}
-                        style={{width, height: height*0.55}} 
+                        source={{uri: image342(movie.poster_path) || fallbackMoviePoster}}
+                        style={{width:width*0.9, height: height*0.55 , marginLeft:width*0.05}} 
                     />
-                    <LinearGradient 
-                        colors={['transparent', 'rgba(23, 23, 23, 0.8)', 'rgba(23, 23, 23, 1)']} 
-                        style={{width, height: height*0.40}}
-                        start={{ x: 0.5, y: 0 }}
-                        end={{ x: 0.5, y: 1 }}
-                        className="absolute bottom-0"
-                    />
+                    
                 </View>
             )
         }
@@ -100,9 +92,9 @@ export default function MovieScreen() {
         
       {/* movie details */}
       
-      <View style={{marginTop: -(height*0.09)}} className="space-y-3">
+      <View style={{marginTop:10}} className="">
         {/* title */}
-        <Text className="text-white text-center text-3xl font-bold tracking-widest">
+        <Text style={{textAlign:'center',fontSize:25}}>
             {
                 movie?.title
             }
@@ -111,30 +103,18 @@ export default function MovieScreen() {
         {/* status, release year, runtime */}
         {
             movie?.id? (
-                <Text className="text-neutral-400 font-semibold text-base text-center">
-                    {movie?.status} • {movie?.release_date?.split('-')[0] || 'N/A'} • {movie?.runtime} min
+                <Text style={{textAlign:'center',marginBottom:20}}>
+                    {movie?.release_date?.split('-')[0] || 'N/A'} , {movie?.runtime} minutes
                 </Text>
             ):null
         }
         
 
         
-        {/* genres  */}
-        <View className="flex-row justify-center mx-4 space-x-2">
-            {
-                movie?.genres?.map((genre,index)=>{
-                    let showDot = index+1 != movie.genres.length;
-                    return (
-                        <Text key={index} className="text-neutral-400 font-semibold text-base text-center">
-                            {genre?.name} {showDot? "•":null}
-                        </Text>
-                    )
-                })
-            }
-        </View>
+        
 
         {/* description */}
-        <Text className="text-neutral-400 mx-4 tracking-wide">
+        <Text style={{padding:10}}>
             {
                 movie?.overview
             }
@@ -144,9 +124,11 @@ export default function MovieScreen() {
         
       
       {/* cast */}
-      {
-        movie?.id && cast.length>0 && <Cast navigation={navigation} cast={cast} />
-      }
+      <Text style={{height: 100, marginTop:10,marginBottom:10}}>
+        {
+            movie?.id && cast.length>0 && <Cast navigation={navigation} cast={cast} />
+        }
+      </Text>
       
       {/* similar movies section */}
       {
